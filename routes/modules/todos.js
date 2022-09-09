266af67399d134ebdecdfab8d89c9809d2f83cff
WIlson <eaz73727@gmail.com>
2022-09-10 02:40:17 +0800

    feat: add new todo func

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -1,8 +1,28 @@
 const express = require('express')
 const router = express.Router()
+const db = require('../../models')
+const Todo = db.Todo
+
+router.post('/', (req, res) => {
+  const UserId = req.user.id
+  const { name } = req.body
+  Todo.create({
+    name,
+    UserId
+  })
+    .then(() => res.redirect('/todos'))
+})
+
+router.get('/new', (req, res) => {
+  res.render('new')
+})
 
 router.get('/', (req, res) => {
-  res.render('index')
+  const UserId = req.user.id
+  Todo.findAll({ where: { UserId }, raw: true, nest: true })
+    .then(todos => {
+      return res.render('index', { todos })
+    })
 })
 
 module.exports = router
