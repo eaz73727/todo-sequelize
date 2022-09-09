c1fe602d1b64f2cc701e01973b502d2310d5ce1a
WIlson <eaz73727@gmail.com>
2022-09-10 01:20:14 +0800

    feat: set up auth middleware

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,10 @@ const express = require('express')
 const router = express.Router()
 const users = require('./modules/users')
 const todos = require('./modules/todos')
+const { authenticator } = require('../middleware/auth')
 
 router.use('/users', users)
-router.use('/todos', todos)
+router.use('/todos', authenticator, todos)
 router.get('/', (req, res) => res.redirect('/todos/'))
 
 module.exports = router
