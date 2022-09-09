5fe891b839e620bc940bd1961e1a38d9de2f641e
WIlson <eaz73727@gmail.com>
2022-09-10 01:09:19 +0800

    feat: set up register func

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const bcrypt = require('bcryptjs')
+const db = require('../../models')
+const User = db.User
 const router = express.Router()
 
 router.get('/login', (req, res) => {
@@ -14,7 +17,43 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
-  res.send('register')
+  const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!name, !email, !password, !confirmPassword) {
+    errors.push({ message: '所有欄位都是必填！' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不符' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email
+    })
+  }
+  User.findOne({ where: { email } })
+    .then(user => {
+      if (user) {
+        errors.push({ message: '此信箱已註冊！' })
+        return res.render('register', { name, email, errors })
+      }
+      bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(password, salt))
+        .then(hash => {
+          User.create({
+            name,
+            email,
+            password: hash
+          })
+        })
+        .then(() => {
+          req.flash('success_msg', "註冊成功，登入以使用！")
+          res.redirect('/')
+        })
+    })
+    .catch(err => console.log(err))
 })
 
 router.get('/logout', (req, res, next) => {
